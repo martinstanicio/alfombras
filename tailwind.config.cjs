3954da773defc8179ffe8adc2fb08bd438aa3e5e
Martín Stanicio <martuytimo@gmail.com>
2023-09-08 20:49:31 -0300

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,26 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    extend: {
+      colors: {
+        text: "#040401",
+        background: "#faf6e6",
+        primary: "#d8c98d",
+        secondary: "#ede6ca",
+        accent: "#004225",
+      },
+      fontFamily: {
+        sans: ["Karla", ...defaultTheme.fontFamily.sans],
+        serif: ["Merriweather", ...defaultTheme.fontFamily.serif],
+      },
+    },
+  },
+  plugins: [],
+};
